8652e135777fcfb61c49b4159a05f17b0c9ab5e9
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-20 05:01:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+#  new_array = []
+ # counter = 0
+  
+#  while counter < array.length do
+#  new_array << array[counter] ** 2
+ #   counter += 1
+ # end
+#new_array
+#end
+
+counter = 0
+array.each {|number| array[counter] ** 2}
+
 end
