9d81df811a44bcbc24b126d21a96f1fab18ce41f
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-20 05:24:02 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,9 @@
 def square_array(numbers)
 
-new_numbers = []
-numbers.each do |num| 
-  new_number << num ** 2
-end
-return new_numbers
-end
 
   new_numbers = []
-end 	  numbers.each do |num|
+end 	  
+    numbers.each do |num|
     new_numbers << (num ** 2)
   end
   return new_numbers
